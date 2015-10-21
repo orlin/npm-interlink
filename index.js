c3bbc7a3e6656651b681605797091f37ec9e4ccb
Orlin M Bozhinov <om@soundsapiens.com>
2015-10-21 20:39:24 +0300

    link the modules, don't even try if package.json not found, also print error details

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import path from 'path'
 import isThere from 'is-there'
 import jsonfile from 'jsonfile'
 import R from 'ramda'
+import {spawnSync} from 'child_process'
 
 let dirList = []
 let modules = {}
 
 if (isThere('.npm-interlink')) {
-  console.log('Configuration .npminter-link found.')
+  console.log('Configuration .npm-interlink found.')
   dirList = fs.readFileSync('.npm-interlink').toString().split('\n').filter(item => {
     return item !== ''
   })
@@ -22,14 +23,32 @@ function keys (something) {
   return R.keys(something) || []
 }
 
+function gotErr (spawned) {
+  if (spawned.status !== 0 || spawned.error || spawned.stderr.toString()) {
+    console.error(R.pick(['status', 'error', 'stderr'], spawned))
+    return true
+  } else {
+    return false
+  }
+}
+
 for (let dir of dirList) {
-  try {
-    let pkg = jsonfile.readFileSync(path.join(dir, 'package.json'))
-    modules[pkg.name] = {}
-    modules[pkg.name].dir = dir
-    modules[pkg.name].links = R.union(keys(pkg.dependencies), keys(pkg.devDependencies))
-  } catch (e) {
-    console.error(e)
+  if (isThere(path.join(dir, 'package.json'))) {
+    try {
+      let pkg = jsonfile.readFileSync(path.join(dir, 'package.json'))
+      modules[pkg.name] = {}
+      modules[pkg.name].dir = dir
+      modules[pkg.name].links = R.union(keys(pkg.dependencies), keys(pkg.devDependencies))
+      console.log(`\$ cd ${dir} && npm link #${pkg.name}`)
+      let res = spawnSync('npm', ['link'], {cwd: dir})
+      if (!gotErr(res)) {
+        console.log(`Linked ${pkg.name}.`)
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  } else {
+    console.log(`Skipping ${dir} - package.json not found.`)
   }
 }
 
@@ -38,7 +57,7 @@ for (let name in modules) {
   modules[name].links = R.intersection(modules[name].links, names)
 }
 
-console.log('Will try to interlink the following modules:')
+console.log('If no errors above, the following should be interlinked:')
 for (let name in modules) {
   console.log(`${name}: [${modules[name].links}]`)
 }
