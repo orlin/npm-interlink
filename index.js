4508c2f43b539b4539f248ce03d8f0a84b67900e
Orlin M Bozhinov <om@soundsapiens.com>
2015-10-21 17:49:11 +0300

    figure out what will be linked and console.log the intent

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import fs from 'fs'
+import path from 'path'
 import isThere from 'is-there'
+import jsonfile from 'jsonfile'
+import R from 'ramda'
 
 let dirList = []
+let modules = {}
 
 if (isThere('.npm-interlink')) {
   console.log('Configuration .npminter-link found.')
@@ -14,5 +18,27 @@ if (isThere('.npm-interlink')) {
   })
 }
 
-console.log('Will try to interlink the following dirs:')
-console.log(dirList)
+function keys (something) {
+  return R.keys(something) || []
+}
+
+for (let dir of dirList) {
+  try {
+    let pkg = jsonfile.readFileSync(path.join(dir, 'package.json'))
+    modules[pkg.name] = {}
+    modules[pkg.name].dir = dir
+    modules[pkg.name].links = R.union(keys(pkg.dependencies), keys(pkg.devDependencies))
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+for (let name in modules) {
+  let names = R.keys(modules)
+  modules[name].links = R.intersection(modules[name].links, names)
+}
+
+console.log('Will try to interlink the following modules:')
+for (let name in modules) {
+  console.log(`${name}: [${modules[name].links}]`)
+}
